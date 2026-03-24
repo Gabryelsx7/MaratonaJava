d385c459f2b5d34ac3f36bf64de8ee6175db968a
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-03-24 18:35:28 -0300

    add

--- src/academy/dev/dojo/maratonajava/javacore/Aintroducaoclasses/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/dev/dojo/maratonajava/javacore/Aintroducaoclasses/Bintroducaometodos/dominio/Calculadora.java
@@ -1,5 +1,8 @@
 package academy.dev.dojo.maratonajava.javacore.Aintroducaoclasses.Bintroducaometodos.dominio;
 
 public class Calculadora {
-    public
+
+    public void somaDoisNumeros(){
+        System.out.println(10+10);
+    }
 }
