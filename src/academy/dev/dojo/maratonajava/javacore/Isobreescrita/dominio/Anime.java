6913f26f9458d752d9acb4aa6481ce18df2ba5d0
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-04-10 18:57:23 -0300

    att

---
+++ src/academy/dev/dojo/maratonajava/javacore/Isobreescrita/dominio/Anime.java
@@ -0,0 +1,4 @@
+package academy.dev.dojo.maratonajava.javacore.Isobreescrita.dominio;
+
+public class Anime {
+}
---
+++ src/academy/dev/dojo/maratonajava/javacore/Isobreescrita/test/AnimeTest01.java
@@ -0,0 +1,4 @@
+package academy.dev.dojo.maratonajava.javacore.Isobreescrita.test;
+
+public class AnimeTest01 {
+}
