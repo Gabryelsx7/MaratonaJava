08c32d3b23a67dc63d58109fe3675cdb24751839
Gabryel C. <gabryelchianca8@gmail.com>
2026-05-06 15:59:59 -0300

    aula122

---
+++ src/academy/dev/dojo/maratonajava/javacore/Rdates/test/InstantTest01.java
@@ -0,0 +1,15 @@
+package academy.dev.dojo.maratonajava.javacore.Rdates.test;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public class InstantTest01 {
+    static void main(String[] args) {
+        Instant now =  Instant.now();
+        System.out.println(now);
+        System.out.println(LocalDateTime.now());
+        System.out.println(now.getEpochSecond());
+        System.out.println(now.getNano());
+        System.out.println(now.ofEpochSecond(3,0));
+    }
+}
