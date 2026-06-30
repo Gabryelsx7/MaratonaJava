73335d7b65be6f5c0943671d5574a912f6029ef1
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-06-29 21:09:30 -0300

    Maratona Java 172 - Coleções pt 12 - Binary Search

--- src/MaratonaJava/maratonajava/javacore/Ycolecoes/test/BinarySearchTest02.java
+++ src/MaratonaJava/maratonajava/javacore/Ycolecoes/test/BinarySearchTest02.java
@@ -4,35 +4,25 @@ import MaratonaJava.maratonajava.javacore.Ycolecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
-class MangaByIdComparator implements Comparator<Manga> {
-    @Override
-    public int compare(Manga manga1, Manga manga2) {
-        return manga1.getId().compareTo(manga2.getId());
-    }
-}
-public class MangaSorteTest01 {
+public class BinarySearchTest02 {
     static void main(String[] args) {
         List<Manga> mangas = new ArrayList<>();
+        MangaByIdComparator mangaByIdComparator = new MangaByIdComparator();
         mangas.add(new Manga(5L, "Pokemon", 19.9));
         mangas.add(new Manga(1L, "Dragon Ball ", 9.5));
         mangas.add(new Manga(4L, "DR Stone", 3.2));
         mangas.add(new Manga(3L, "Attack on Titan", 11.20));
         mangas.add(new Manga(2L, "Bersek", 2.99));
 
+
+        //Collections.sort(mangas);
+        mangas.sort(mangaByIdComparator);
         for (Manga manga : mangas) {
             System.out.println(manga);
         }
-        Collections.sort(mangas);
-        System.out.println("------------");
-        for (Manga manga : mangas) {
-            System.out.println(manga);
-        }
-        mangas.sort(new MangaByIdComparator());
-        System.out.println("------------");
-        for (Manga manga : mangas) {
-            System.out.println(manga);
+        Manga mangaToSearch = new Manga(1L, "Dragon Ball ", 9.5);
+        System.out.println(Collections.binarySearch(mangas,mangaToSearch, mangaByIdComparator));
     }
-}}
+}
