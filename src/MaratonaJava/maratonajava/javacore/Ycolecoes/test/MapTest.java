b1364dfaad2216584f44ca9f120a52afd7216c6f
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-07-02 22:39:01 -0300

    Maratona Java 179 - Coleções pt 19 - Map, HashMap, LinkedHashMap pt 02

---
+++ src/MaratonaJava/maratonajava/javacore/Ycolecoes/test/MapTest.java
@@ -0,0 +1,29 @@
+package MaratonaJava.maratonajava.javacore.Ycolecoes.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+    static void main(String[] args) {
+        Map<String , String> map = new HashMap<>();
+
+        map.put("Teklado" ,"Teclado");
+        map.put("mouze" ,"mouse");
+        map.put("vc" ,"você");
+        System.out.println(map);
+
+        for (String key : map.keySet()){
+            System.out.println(key);
+        }
+        System.out.println("----------");
+
+        for (String value : map.values()){
+            System.out.println(value);
+        }
+
+        System.out.println("--------------");
+        for (Map.Entry<String, String> entry : map.entrySet()){
+            System.out.println(entry.getKey()+ " - " +entry.getValue());
+        }
+    }
+}
---
+++ src/MaratonaJava/maratonajava/javacore/Ycolecoes/test/MapTest02.java
@@ -0,0 +1,31 @@
+package MaratonaJava.maratonajava.javacore.Ycolecoes.test;
+
+import MaratonaJava.maratonajava.javacore.Ycolecoes.dominio.Consumidor;
+import MaratonaJava.maratonajava.javacore.Ycolecoes.dominio.Manga;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest02 {
+    static void main(String[] args) {
+        Consumidor c1 = new Consumidor("WILLIAN");
+        Consumidor c2 = new Consumidor("Gabryel");
+        System.out.println(c1);
+
+        Manga manga1 = new Manga(5L, "Pokemon", 19.9);
+        Manga manga2 = new Manga(1L, "Dragon Ball ", 9.5);
+        Manga manga3 = new Manga(4L, "DR Stone", 3.2);
+        Manga manga4 = new Manga(3L, "Attack on Titan", 11.20);
+        Manga manga6 = new Manga(2L, "Bersek", 2.99);
+
+        Map<Consumidor , Manga> consumidorManga = new HashMap<>();
+
+        consumidorManga.put(c1 , manga1);
+        consumidorManga.put(c2 , manga4);
+
+        for (Map.Entry<Consumidor , Manga> entry : consumidorManga.entrySet()){
+            System.out.println(entry.getKey().getNome()+ " - " +entry.getValue().getNome());
+
+    }
+}
+}
