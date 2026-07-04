6c43bb08e6ad3549f0dc2c1234f961c9969f98e7
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-07-04 17:08:07 -0300

    Maratona Java 185 - Generics pt 03 - Wildcard pt 02

--- src/MaratonaJava/maratonajava/javacore/Zgenerics/test/WildCardtest02.java
+++ src/MaratonaJava/maratonajava/javacore/Zgenerics/test/WildCardtest02.java
@@ -7,12 +7,20 @@ public class WildCardtest02 {
     static void main(String[] args) {
         List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
         List<Gato> gatos = List.of(new Gato(), new Gato());
+        printConsulta(gatos);
+        printConsulta(cachorros);
 
     }
-    private static void printConsulta(List<Animal>[] animals){
-        for (Animal animal : (Animal[]) animals) {
+
+    private static void printConsulta(List<? extends Animal> animals) {
+        for (Animal animal : animals) {
             animal.consulta();
         }
-        
+    }
+
+    private static void printConsultaAnimal(List<? super Animal> animals) {
+        animals.add(new Cachorro());
+        animals.add(new Gato());
+
     }
 }
