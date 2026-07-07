7631e499bfe38917973ef61d6382f5c83a230627
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-07-07 16:52:33 -0300

    Maratona Java 191 - Classes Internas pt 03 - Classes Anônimas

---
+++ src/MaratonaJava/maratonajava/javacore/ZZAclassesinternas/test/AnonymosClassesTest01.java
@@ -0,0 +1,19 @@
+package MaratonaJava.maratonajava.javacore.ZZAclassesinternas.test;
+
+
+class Animal{
+    public void walk(){
+        System.out.println("ANIMAL WALKING");
+    }
+}
+public class AnonymosClassesTest01 {
+    static void main(String[] args) {
+        Animal animal = new Animal(){
+            @Override
+            public void walk() {
+                System.out.println("Walking in the shadows");
+            }
+        };
+        animal.walk();
+    }
+}
