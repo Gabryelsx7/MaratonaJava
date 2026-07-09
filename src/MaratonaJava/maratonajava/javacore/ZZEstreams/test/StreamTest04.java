ed003dd69821604cd8b0521859476d89ec0f7d61
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-07-09 20:50:56 -0300

    Maratona Java 206 - Streams pt 04 - FlatMap pt 01

---
+++ src/MaratonaJava/maratonajava/javacore/ZZEstreams/test/StreamTest04.java
@@ -0,0 +1,23 @@
+package MaratonaJava.maratonajava.javacore.ZZEstreams.test;
+
+import MaratonaJava.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamTest04 {
+    static void main(String[] args) {
+        List<List<String>> devDojo = new ArrayList<>();
+        List<String> grapicDesigners = List.of("Wildnei Suane", "Catarina Santos", "Sandy Carolina");
+        List<String> desevolpers = List.of("William", "David", "Harrison");
+        List<String> studentes = List.of("Édipo", "Gustavo Mendes", "Guilherme");
+        devDojo.add(grapicDesigners);
+        devDojo.add(desevolpers);
+        devDojo.add(studentes);
+
+        devDojo.stream().flatMap(Collection::stream).forEach(System.out::println);
+
+    }
+}
