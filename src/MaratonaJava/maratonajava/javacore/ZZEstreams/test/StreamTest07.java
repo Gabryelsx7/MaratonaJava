622af3e7f54a4b0dfdc9e553ac6f5a3a2317e4b7
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-07-11 14:56:46 -0300

    Maratona Java 209 - Streams pt 07 - Reduce pt 01

---
+++ src/MaratonaJava/maratonajava/javacore/ZZEstreams/test/StreamTest07.java
@@ -0,0 +1,28 @@
+package MaratonaJava.maratonajava.javacore.ZZEstreams.test;
+
+import MaratonaJava.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class StreamTest07 {
+    static void main(String[] args) {
+    List<Integer> integers = List.of(1,2,3,4,5,6);
+
+        integers.stream().reduce((x,y)->x+y).ifPresent(System.out::println);
+        System.out.println(integers.stream().reduce(0,(x,y) ->x+y));
+
+        integers.stream().reduce(Integer::sum).ifPresent(System.out::println);
+        System.out.println(integers.stream().reduce(0,Integer::sum));
+
+        System.out.println("-----------------------");//mutiplicar
+        integers.stream().reduce((x,y)->x*y).ifPresent(System.out::println);
+        System.out.println(integers.stream().reduce(0,(x,y) ->x*y));
+
+        System.out.println("-----------------------");//pegar o maximo da lista
+        integers.stream().reduce((x,y)-> x>y ? x: y).ifPresent(System.out::println);
+        System.out.println(integers.stream().reduce(0,  Integer::max));
+
+    }
+}
